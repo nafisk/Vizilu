3202d7f0707c150096a0ba5ef1ba5985c2b11b83
Nafis Khan <49691160+nkhan-godaddy@users.noreply.github.com>
2023-11-17 19:50:39 -0500

    Refactor frame component and package name

--- pages/FrameScreen.js
+++ pages/FrameScreen.js
@@ -1,75 +1,66 @@
-import { Ionicons } from '@expo/vector-icons'; // Make sure you have @expo/vector-icons installed
+import { Ionicons } from '@expo/vector-icons';
 import * as ScreenOrientation from 'expo-screen-orientation';
 import { Accelerometer } from 'expo-sensors';
-import { throttle } from 'lodash'; // Ensure lodash is installed
-import React, { useEffect, useState } from 'react';
-import {
-  Dimensions,
-  Image,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
-import frames from '../assets/Frames'; 
+import { throttle } from 'lodash';
+import React, { memo, useEffect, useState } from 'react';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
+import Frame480p from '../assets/Frame480p';
+import Frame720p from '../assets/Frame720p';
+import Frame1080p from '../assets/Frame1080p';
+import styles from './FrameScreenStyles'; // Import styles for the component
 
-const { width, height } = Dimensions.get('window');
-UPDATE_IN_MS = 30;
+const UPDATE_IN_MS = 10; // Time interval for accelerometer updates
+const THROTTLE_INTERVAL = 5; // Interval to limit how often frame updates can occur
 
-const FrameScreen = () => {
-  const frameKeys = Object.keys(frames).sort();
+// FrameScreen component displays images based on device orientation
+const FrameScreen = memo(() => {
+  // Extract and sort keys from Frame480p object
+  const frameKeys = Object.keys(Frame480p).sort(
+    (a, b) => parseInt(a) - parseInt(b)
+  );
   const middleIndex = Math.floor(frameKeys.length / 2);
 
-  const [data, setData] = useState({ x: 0, y: 0, z: 0 });
+  // State for the current frame and screen orientation
   const [currentFrame, setCurrentFrame] = useState(
-    frames[frameKeys[middleIndex]]
+    Frame480p[frameKeys[middleIndex]]
   );
   const [orientation, setOrientation] = useState(
     ScreenOrientation.Orientation.PORTRAIT_UP
   );
   const isPortrait = orientation === ScreenOrientation.Orientation.PORTRAIT_UP;
 
-  // Preload images
   useEffect(() => {
-    frameKeys.forEach(key => {
-      Image.prefetch(frames[key]);
-    });
-  }, []);
-
-  useEffect(() => {
+    // Setting the update interval for the accelerometer
     Accelerometer.setUpdateInterval(UPDATE_IN_MS);
 
+    // Throttle frame updates to improve performance
     const throttledSetFrame = throttle(data => {
-      const frameIndex = mapTiltToFrameIndex(
-        orientation === ScreenOrientation.Orientation.PORTRAIT_UP
-          ? data.x
-          : data.y
-      );
-      setCurrentFrame(frames[frameKeys[frameIndex]]);
-    }, 200); // Throttle updates to every 200ms
+      const frameIndex = mapTiltToFrameIndex(isPortrait ? data.x : data.y);
+      setCurrentFrame(Frame480p[frameKeys[frameIndex]]);
+    }, THROTTLE_INTERVAL);
 
-    const subscription = Accelerometer.addListener(accelerometerData => {
-      setData(accelerometerData);
-      throttledSetFrame(accelerometerData);
-    });
+    // Add accelerometer listener
+    const subscription = Accelerometer.addListener(throttledSetFrame);
 
+    // Cleanup on unmount
     return () => {
       subscription.remove();
       throttledSetFrame.cancel();
     };
   }, [orientation]);
 
+  // Maps device tilt to the appropriate frame index
   const mapTiltToFrameIndex = tilt => {
     const sensitivity = 2;
     const maxTilt = 1;
     const tiltRatio = (tilt * sensitivity + maxTilt) / (2 * maxTilt);
-    const frameIndex = Math.min(
+    return Math.min(
       Math.max(Math.round((frameKeys.length - 1) * tiltRatio), 0),
       frameKeys.length - 1
     );
-    return frameIndex;
   };
 
+  // Toggle screen orientation between portrait and landscape
   const toggleOrientation = async () => {
     const currentOrientation = await ScreenOrientation.getOrientationAsync();
     if (currentOrientation === ScreenOrientation.Orientation.PORTRAIT_UP) {
@@ -86,6 +77,7 @@ const FrameScreen = () => {
   };
 
   return (
+    // Render the current frame and controls for orientation
     <View style={styles.container}>
       <Image
         source={currentFrame}
@@ -101,9 +93,9 @@ const FrameScreen = () => {
             <Text style={styles.orientationButtonText}>Toggle Orientation</Text>
           </TouchableOpacity>
           <View style={styles.sensorDataContainer}>
-            <Text style={styles.sensorDataText}>X: {data.x.toFixed(3)}</Text>
-            <Text style={styles.sensorDataText}>Y: {data.y.toFixed(3)}</Text>
-            <Text style={styles.sensorDataText}>Z: {data.z.toFixed(3)}</Text>
+            <Text style={styles.sensorDataText}>
+              Tilt the device to view different frames
+            </Text>
           </View>
         </>
       ) : (
@@ -116,60 +108,6 @@ const FrameScreen = () => {
       )}
     </View>
   );
-};
-
-const styles = StyleSheet.create({
-  frameImageLandscape: {
-    width: height,
-    height: width,
-    resizeMode: 'contain',
-  },
-  returnButton: {
-    position: 'absolute',
-    top: 20,
-    left: 20,
-    backgroundColor: 'rgba(0,0,0,0.7)',
-    padding: 10,
-    borderRadius: 5,
-    zIndex: 10,
-  },
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
-  },
-  frameImage: {
-    width: '100%', // Use the full width of the screen
-    height: height * 0.5625, // Adjust the height according to the aspect ratio of the image
-    resizeMode: 'contain', // Ensure the entire image is visible within the frame
-    marginBottom: 20,
-  },
-  text: {
-    fontSize: 18,
-    marginBottom: 10,
-  },
-  sensorDataContainer: {
-    flexDirection: 'row', // Align items in a row
-    justifyContent: 'space-around', // Distribute space evenly around items
-    width: '100%', // Take the full width of the container
-    marginBottom: 20, // Add some margin at the bottom
-  },
-  sensorDataText: {
-    fontSize: 16,
-    color: 'gray',
-  },
-  orientationButton: {
-    backgroundColor: 'rgba(0,0,0,0.7)',
-    padding: 10,
-    borderRadius: 5,
-    zIndex: 10,
-    marginBottom: 20,
-  },
-  orientationButtonText: {
-    color: '#fff',
-    fontSize: 16,
-  },
 });
 
 export default FrameScreen;
