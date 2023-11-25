36c9fb8a71ef334aac282a4475ad231a9b9e86c0
Nafis Khan <49691160+nkhan-godaddy@users.noreply.github.com>
2023-11-25 16:12:56 -0500

    Add shop layout

---
+++ mock/product.js
@@ -0,0 +1,35 @@
+const mockData = {
+  id: 1,
+  name: 'Vizilu',
+  description:
+    'Vizilu 3D Picture Frame (12"x18") - Immersive Photo Display, Reverse Perspective Illusion, Perfect Wall Art, Novelty Gift',
+  images: [
+    {
+      url: require('../assets/Shop/1.jpeg'),
+      alt: 'Image 1',
+    },
+    {
+      url: require('../assets/Shop/2.jpeg'),
+      alt: 'Image 2',
+    },
+    {
+      url: require('../assets/Shop/3.jpeg'),
+      alt: 'Image 3',
+    },
+    {
+      url: require('../assets/Shop/4.jpeg'),
+      alt: 'Image 4',
+    },
+    {
+      url: require('../assets/Shop/5.jpeg'),
+      alt: 'Image 5',
+    },
+    {
+      url: require('../assets/Shop/6.jpeg'),
+      alt: 'Image 6',
+    },
+  ],
+  price: 49.99,
+};
+
+export default mockData;
--- pages/ShopScreen/ShopScreen.js
+++ pages/ShopScreen/ShopScreen.js
@@ -1,23 +1,99 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Dimensions,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import mockData from '../../mock/product';
+
+const { width } = Dimensions.get('window');
 
 const ShopScreen = () => {
+  const { images, videos, name, description, price } = mockData;
+
+  const handleBuyNow = () => {
+    // Handle the buy now action
+    console.log('Buy Now clicked');
+  };
+
   return (
-    <View style={styles.center}>
-      <Text style={styles.text}>Shop</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        {images.map((image, index) => (
+          <View key={index} style={styles.imageContainer}>
+            <Image
+              source={image.url}
+              style={styles.image}
+              resizeMode='contain'
+            />
+          </View>
+        ))}
+      </ScrollView>
+      <Text style={styles.title}>{name}</Text>
+      <Text style={styles.description}>{description}</Text>
+      <Text style={styles.price}>${price}</Text>
+
+      {/* Buy Now Button */}
+      <TouchableOpacity style={styles.buyButton} onPress={handleBuyNow}>
+        <Text style={styles.buyButtonText}>Buy Now</Text>
+      </TouchableOpacity>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  center: {
+  container: {
     flex: 1,
-    justifyContent: 'center',
+    padding: 10,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  description: {
+    fontSize: 16,
+    marginBottom: 10,
+  },
+  price: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  imageContainer: {
+    width: width - 20,
     alignItems: 'center',
     backgroundColor: '#fff',
   },
-  text: {
-    fontSize: 24,
+  image: {
+    width: width,
+    height: 200,
+  },
+  videoContainer: {
+    width: width - 20,
+    height: 200,
+    marginRight: 10,
+    alignItems: 'center',
+  },
+  video: {
+    width: '100%',
+    height: '100%',
+  },
+  buyButton: {
+    backgroundColor: '#007bff', // Example color
+    padding: 15,
+    borderRadius: 5,
+    marginTop: 20,
+    alignItems: 'center',
+  },
+  buyButtonText: {
+    color: '#ffffff',
+    fontSize: 18,
+    fontWeight: 'bold',
   },
 });
 
