41895e95b6c89b9d1d376a28770100a54afda2d5
nafisk <nafisrizwank@gmail.com>
2023-11-27 18:20:21 -0500

    Add user profile display and edit profile functionality

--- pages/SettingsScreen/SettingsScreen.js
+++ pages/SettingsScreen/SettingsScreen.js
@@ -12,6 +12,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { CommonActions } from '@react-navigation/native';
 import { CognitoUserPool } from 'amazon-cognito-identity-js'; // Import CognitoUserPool
 import logo from '../../assets/app/logo.webp';
+import dummyPfp from '../../assets/app/dummy.webp';
 
 import userPoolConfig from '../../cognitoConfig'; // Assuming this is your Cognito config
 const userPool = new CognitoUserPool(userPoolConfig);
@@ -19,6 +20,7 @@ const userPool = new CognitoUserPool(userPoolConfig);
 const SettingsScreen = ({ navigation }) => {
   const [isEnabled, setIsEnabled] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userInfo, setUserInfo] = useState(null); // State to store user info
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -43,11 +45,27 @@ const SettingsScreen = ({ navigation }) => {
           setIsLoggedIn(false);
         } else {
           setIsLoggedIn(true);
+          fetchUserInfo(currentUser);
         }
       });
     } else {
       setIsLoggedIn(false);
     }
+  };
+
+  const fetchUserInfo = currentUser => {
+    currentUser.getUserAttributes((err, attributes) => {
+      if (err) {
+        console.log('Error fetching user info:', err);
+        return;
+      }
+      const userData = attributes.reduce((acc, attr) => {
+        acc[attr.getName()] = attr.getValue();
+        return acc;
+      }, {});
+      setUserInfo(userData);
+      console.log(userData);
+    });
   };
 
   const toggleSwitch = () => setIsEnabled(previousState => !previousState);
@@ -65,6 +83,10 @@ const SettingsScreen = ({ navigation }) => {
       })
     );
   };
+  const handleEditProfile = () => {
+    // Implement navigation or action for editing profile
+    console.log('Edit Profile');
+  };
 
   const handleLogin = () => {
     navigation.dispatch(
@@ -77,18 +99,35 @@ const SettingsScreen = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
-      {/* User Profile */}
-      <View style={styles.settingItem}>
-        <Text style={styles.settingText}>User Profile</Text>
-        {/* Add navigation or action */}
-      </View>
+      {/* Display User Info if Logged In */}
+      {isLoggedIn && userInfo && (
+        <View style={styles.userInfoContainer}>
+          <Image source={dummyPfp} style={styles.profilePic} />
+          <Text style={styles.userInfoText}>
+            Welcome, {userInfo['family_name']}
+          </Text>
+          <Text style={styles.userDetailText}>Email: {userInfo['email']}</Text>
+          <Text style={styles.userDetailText}>
+            Birthdate: {userInfo['birthdate']}
+          </Text>
+          <Text style={styles.userDetailText}>
+            Address: {userInfo['address']}
+          </Text>
+          <TouchableOpacity
+            style={styles.editProfileButton}
+            onPress={handleEditProfile}
+          >
+            <Text style={styles.editProfileButtonText}>Edit Profile</Text>
+          </TouchableOpacity>
+        </View>
+      )}
 
       {/* Notifications */}
       <View style={styles.settingItem}>
         <Text style={styles.settingText}>Notifications</Text>
         <Switch
-          trackColor={{ false: '#767577', true: '#81b0ff' }}
-          thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+          trackColor={{ false: '#767577', true: '#f88c38' }}
+          thumbColor={isEnabled ? '#f5dd4b' : '#767577'}
           ios_backgroundColor='#3e3e3e'
           onValueChange={toggleSwitch}
           value={isEnabled}
@@ -140,7 +179,7 @@ const styles = StyleSheet.create({
   },
   logoutButton: {
     marginTop: 20,
-    backgroundColor: 'blue',
+    backgroundColor: '#198eca',
     padding: 15,
     borderRadius: 5,
     alignItems: 'center',
@@ -172,6 +211,38 @@ const styles = StyleSheet.create({
   },
   headerTitle: {
     fontSize: 18,
+  },
+  userInfoContainer: {
+    padding: 10,
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    marginBottom: 20,
+  },
+  profilePic: {
+    width: 80,
+    height: 80,
+    borderRadius: 40, // Makes it circular
+    marginBottom: 10,
+  },
+  userInfoText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  userDetailText: {
+    fontSize: 16,
+    marginBottom: 3,
+  },
+  editProfileButton: {
+    backgroundColor: '#198eca',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 10,
+  },
+  editProfileButtonText: {
+    color: '#ffffff',
+    fontSize: 16,
   },
 });
 
